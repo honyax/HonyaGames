c3c370aac5cc92aff20b1f697d26da0dc59f2e69
DESKTOP-O6174U3\honya <honyax@gmail.com>
2022-05-18 03:08:18 +0900

    HonyanCraftTest 10 ブロック生成位置を可視化する

--- HonyanCraftTest/Assets/_HonyanCraft/Scripts/Player.cs
+++ HonyanCraftTest/Assets/_HonyanCraft/Scripts/Player.cs
@@ -4,22 +4,39 @@ using Photon.Pun;
 
 public class Player : MonoBehaviourPun
 {
+    private Transform _cachedTransform;
+    private Transform _blockPosition;
+    private GameObject _block;
+
+    private void Start()
+    {
+        _cachedTransform = transform;
+        _blockPosition = _cachedTransform.Find("BlockPosition");
+        _block = _blockPosition.Find("Cell2").gameObject;
+        var mat = _block.GetComponent<MeshRenderer>().material;
+        var color = mat.color;
+        color.a = 0.1f;
+        mat.color = color;
+        _block.SetActive(false);
+    }
+
     private void Update()
     {
         if (!photonView.IsMine)
             return;
 
+        var pos = _cachedTransform.position;
+        var blockPos = Vector3Int.CeilToInt(pos);
+        _blockPosition.position = blockPos;
+        _blockPosition.rotation = Quaternion.identity;
+
         if (Mouse.current.leftButton.wasPressedThisFrame)
         {
-            var pos = transform.position;
-            var blockPos = Vector3Int.CeilToInt(pos);
             PhotonController.Instance.photonView.RPC(nameof(PhotonController.CreateBlockRequest),
                 RpcTarget.MasterClient, blockPos.x, blockPos.y, blockPos.z, Game.Instance.CurrentBlockIndex);
         }
         else if (Mouse.current.rightButton.wasPressedThisFrame)
         {
-            var pos = transform.position;
-            var blockPos = Vector3Int.CeilToInt(pos);
             PhotonController.Instance.photonView.RPC(nameof(PhotonController.DeleteBlockRequest),
                 RpcTarget.MasterClient, blockPos.x, blockPos.y, blockPos.z);
         }
@@ -30,6 +47,10 @@ public class Player : MonoBehaviourPun
         else if (Keyboard.current.zKey.wasPressedThisFrame)
         {
             Game.Instance.ChangeBlockIndex(false);
+        }
+        else if (Keyboard.current.cKey.wasPressedThisFrame)
+        {
+            _block.SetActive(!_block.activeSelf);
         }
     }
 }
