c39dbeb972dd71d72b94b4f140dd8ec7606aafab
DESKTOP-O6174U3\honya <honyax@gmail.com>
2022-06-03 02:36:15 +0900

    Honyaversi 02 Game, Stoneスクリプト作成、初期化で8x8のStoneを生成

---
+++ HonyaversiTest/Assets/_Honyaversi/Scripts/Game.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    [SerializeField]
+    private Stone _stonePrefab;
+
+    [SerializeField]
+    private Transform _stoneBase;
+
+    private Stone[][] _stones;
+
+    private void Start()
+    {
+        _stones = new Stone[8][];
+        for (var z = 0; z < 8; z++)
+        {
+            _stones[z] = new Stone[8];
+            for (var x = 0; x < 8; x++)
+            {
+                var stone = Instantiate(_stonePrefab, _stoneBase);
+                var t = stone.transform;
+                t.localPosition = new Vector3(x * 10, 0, z * 10);
+                t.localRotation = Quaternion.identity;
+                t.localScale = Vector3.one;
+                _stones[z][x] = stone;
+            }
+        }
+    }
+}
