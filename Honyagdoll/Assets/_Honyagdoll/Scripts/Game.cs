9f2e76ed2235057b11d6b9e7bfeae01e1a280613
DESKTOP-O6174U3\honya <honyax@gmail.com>
2022-06-22 00:14:13 +0900

    Honyagdoll XX 動画に合わせた形で作りを修正。

--- Honyagdoll/Assets/_Honyagdoll/Scripts/Game.cs
+++ Honyagdoll/Assets/_Honyagdoll/Scripts/Game.cs
@@ -2,10 +2,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class HonyagdollPlayer : MonoBehaviour
+public class Game : MonoBehaviour
 {
     [SerializeField]
-    private Honyagdoll[] _honyagdollPrefab;
+    private Transform _player;
+
+    [SerializeField]
+    private Honyagdoll[] _honyagdollPrefabs;
 
     [SerializeField]
     private ParticleSystem _explosionEffect;
@@ -19,41 +22,33 @@ public class HonyagdollPlayer : MonoBehaviour
     [SerializeField]
     private AudioClip _explosionSe;
 
-    private float _explosionForce = 1000;
-
-    private float _explosionRadius = 100;
-
-    private float _upwardsModifier = 5;
-
-    private ForceMode _forceMode = ForceMode.Impulse;
+    private System.Random _random = new System.Random();
 
     private List<Honyagdoll> _honyagdolls = new List<Honyagdoll>();
 
-    private System.Random _random = new System.Random();
-
-    void Update()
+    private void Update()
     {
+        var kb = Keyboard.current;
         var mouse = Mouse.current;
         if (mouse.leftButton.wasReleasedThisFrame)
         {
-            var ps = Instantiate(_explosionEffect, this.transform);
-            var t = ps.gameObject.transform;
+            var ps = Instantiate(_explosionEffect, _player);
+            var t = ps.transform;
             t.localPosition = new Vector3(0, 1, 0);
             t.localRotation = Quaternion.identity;
             t.localScale = Vector3.one * 2;
             foreach (var honyagdoll in _honyagdolls)
             {
-                honyagdoll.MainRigidbody.AddExplosionForce(_explosionForce, gameObject.transform.position, _explosionRadius, _upwardsModifier, _forceMode);
+                honyagdoll.MainRigidbody.AddExplosionForce(1000, _player.position, 100, 5, ForceMode.Impulse);
             }
             _seAudioSource.PlayOneShot(_explosionSe);
         }
         else if (mouse.rightButton.wasReleasedThisFrame)
         {
-            var honyagdoll = Instantiate(_honyagdollPrefab[_random.Next(_honyagdollPrefab.Length)]);
+            var honyagdoll = Instantiate(_honyagdollPrefabs[_random.Next(_honyagdollPrefabs.Length)]);
             var t = honyagdoll.transform;
-            t.position = transform.position + transform.forward * 5 + Vector3.up * 5;
+            t.position = _player.position + _player.forward * 5 + Vector3.up * 5;
             t.rotation = Quaternion.identity;
-            t.localScale = Vector3.one;
             _honyagdolls.Add(honyagdoll);
             _seAudioSource.PlayOneShot(_spawnSe);
         }
